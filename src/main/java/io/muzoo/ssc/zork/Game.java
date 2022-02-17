590c787d9b6e5dbe85e15edb520ea73216090153
DrNattapoom <nattapoom.dr@gmail.com>
2022-02-17 22:04:40 +0700

    add Command interface and ExitCommand class for exiting the game

--- src/main/java/io/muzoo/ssc/zork/Game.java
+++ src/main/java/io/muzoo/ssc/zork/Game.java
@@ -1,20 +1,31 @@
 package io.muzoo.ssc.zork;
 
+import io.muzoo.ssc.zork.command.ExitCommand;
+
 import java.util.Scanner;
 
 public class Game {
 
+    private Scanner scanner = new Scanner(System.in);
+    private boolean exit = false;
+
+    public boolean isExit() {
+        return this.exit;
+    }
+
+    public void exit() {
+        this.exit = true;
+        this.scanner.close();
+    }
+
     public void start() {
         System.out.println("Game Started");
-        Scanner scanner = new Scanner(System.in);
-        while (scanner.hasNextLine()) {
+        while (!isExit() && scanner.hasNextLine()) {
             String rawInput = scanner.nextLine();
             System.out.println("you enter " + rawInput);
-            if (rawInput.equals("exit")) {
-                break;
-            }
+            ExitCommand exitCommand = new ExitCommand();
+            exitCommand.execute(this);
         }
-        scanner.close();
     }
 
 }
---
+++ src/main/java/io/muzoo/ssc/zork/command/Command.java
@@ -0,0 +1,9 @@
+package io.muzoo.ssc.zork.command;
+
+import io.muzoo.ssc.zork.Game;
+
+public interface Command {
+
+    void execute(Game game);
+
+}
