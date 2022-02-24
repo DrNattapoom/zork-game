ec5d36a09d536b4caa2d511068d9784a905ca160
DrNattapoom <nattapoom.dr@gmail.com>
2022-02-24 22:31:48 +0700

    add DropCommand class

--- src/main/java/io/muzoo/ssc/zork/command/CommandType.java
+++ src/main/java/io/muzoo/ssc/zork/command/CommandType.java
@@ -7,6 +7,7 @@ public enum CommandType {
     INFO(InfoCommand.class, "info", "print out information of the player and the room that the player is currently in, this command only available while playing game"),
     TAKE(TakeCommand.class, "take", "take command is used to pick up the item in the current room, this command only available while playing game"),
     USE(UseCommand.class, "use", "use command is used to use the item that is not weapon in the current room, this command only available while playing game"),
+    DROP(DropCommand.class, "drop", "drop item of choice that the player currently carries, this command only available while playing game"),
     GO(GoCommand.class, "go", "move player to the room as specified by the direction, e.g. north, east, west, south, this command only available while playing game"),
     MAP(MapCommand.class, "map", "print 2D map using ascii art, this command only available while playing game"),
     HELP(HelpCommand.class, "help", "print all commands"),
---
+++ src/main/java/io/muzoo/ssc/zork/command/impl/DropCommand.java
@@ -0,0 +1,26 @@
+package io.muzoo.ssc.zork.command.impl;
+
+import io.muzoo.ssc.zork.Game;
+import io.muzoo.ssc.zork.command.Command;
+import io.muzoo.ssc.zork.map.item.Item;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class DropCommand implements Command {
+
+    @Override
+    public void execute(Game game, String argument) {
+        System.out.println("What do you want to drop?");
+        List<Item> items = game.getPlayer().getItems();
+        for (Item item : items) {
+            System.out.print(item + " ");
+        }
+        System.out.println();
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println("dropping " + input + " ...");
+        items.removeIf(item -> item.getName().equals(input));
+    }
+
+}
