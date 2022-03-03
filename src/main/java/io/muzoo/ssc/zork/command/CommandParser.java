456d2460f3dba8fdb4610022c544d763ec5f86e2
DrNattapoom <nattapoom.dr@gmail.com>
2022-03-03 22:59:31 +0700

    fix command parsing problem

--- src/main/java/io/muzoo/ssc/zork/command/CommandParser.java
+++ src/main/java/io/muzoo/ssc/zork/command/CommandParser.java
@@ -7,7 +7,8 @@ public class CommandParser {
     public static CommandLine parseCommand(String rawInput) {
         for (CommandType commandType : CommandType.values()) {
             if (commandType.match(rawInput)) {
-                String argument = rawInput.replace(commandType.getCommandWord(), "").trim();
+                // String argument = rawInput.replace(commandType.getCommandWord(), "").trim();
+                String argument = StringUtils.replace(rawInput, commandType.getCommandWord(), "", 1).trim();
                 argument = (StringUtils.isBlank(argument)) ? null : argument;
                 return new CommandLine(commandType, argument);
             }
